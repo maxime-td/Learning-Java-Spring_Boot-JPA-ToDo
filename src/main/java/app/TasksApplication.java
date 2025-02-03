d4d3a159c00dbe2818e171ca015d65262999e2bf
Maxime TP <maxime.thoumin@protonmail.com>
2025-02-03 10:57:51 +0100

    Création des tables

--- src/main/java/app/TasksApplication.java
+++ src/main/java/app/TasksApplication.java
@@ -31,8 +31,46 @@ public class TasksApplication implements CommandLineRunner {
 
         List<String> list = Arrays.asList(strings);
         if (list.contains("install")) {
-            // le code de création de la base
-            // voir ci-dessous
+
+            // On supprime les tables si elles existent déjà
+            jdbcTemplate.execute("DROP TABLE tasks IF EXISTS");
+            jdbcTemplate.execute("DROP TABLE categories IF EXISTS");
+
+
+            // On crée la table des catégories
+            jdbcTemplate.execute(
+                    "CREATE TABLE categories ("+
+                            "category_id IDENTITY PRIMARY KEY," +
+                            "name VARCHAR(20) DEFAULT '' "+
+                            ");" );
+            log.info("categories TABLE CREATED");
+
+            jdbcTemplate.update("INSERT INTO categories(name) values('todo'); ");
+            jdbcTemplate.update("INSERT INTO categories(name) values('wip');  ");
+            jdbcTemplate.update("INSERT INTO categories(name) values('done'); ");
+            log.info("categories TABLE POPULATED");
+
+
+            // On crée la table des tâches
+            jdbcTemplate.execute(
+                    "CREATE TABLE tasks (" +
+                            "   task_id       IDENTITY PRIMARY KEY," +
+                            "   category      INTEGER NOT NULL," +
+                            "   content       VARCHAR(500) NOT NULL," +
+                            "   creation_date DATE DEFAULT CURRENT_DATE(), " +
+                            "   end_date      DATE DEFAULT NULL, " +
+                            "   FOREIGN KEY(category) REFERENCES categories(category_id)"+
+                            ");");
+            log.info("tasks TABLE CREATED");
+
+            // un peu de data manipulation language ou DML
+            jdbcTemplate.update("INSERT INTO tasks (category, content) values(3, 'finir le tp 1'); ");
+            jdbcTemplate.update("INSERT INTO tasks (category, content) values(2, 'finir le tp 2'); ");
+            jdbcTemplate.update("INSERT INTO tasks (category, content) values(1, 'finir le tp 3'); ");
+            jdbcTemplate.update("INSERT INTO tasks (category, content) values(1, 'finir le tp 3'); ");
+            log.info("tasks TABLE POPULATED");
         }
+
+
     }
 }
